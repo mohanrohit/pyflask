fcc568217c5c7d4867dbe0183df43fa4fc2bdded
Rohit Mohan <rohit.mohan@gmail.com>
2014-08-28 10:08:35 -0400

    Added template for Hello 1.1. Changed views.py for handling the 1.1 URL using HTML template

--- hello/app/views.py
+++ hello/app/views.py
@@ -1,5 +1,14 @@
 from app import app
 
+from flask import render_template
+
+@app.route("/")
+@app.route("/hello/1/0")
 @app.route("/hello-1.0")
 def hello_1_0():
     return "Hello World"
+
+@app.route("/hello/1/1")
+@app.route("/hello-1.1")
+def hello_1_1():
+    return render_template("hello-1.1.html")
